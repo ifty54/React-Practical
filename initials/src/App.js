5bf257a419b94727efbfe40920afd61b1c11ab17
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-04-30 16:58:29 +0600

    updated

--- initials/src/App.js
+++ initials/src/App.js
@@ -3,9 +3,11 @@ import Greet from './Components/Greet'
 import Tagline from './Components/Tagline'
 import Message from './Components/Message'
 import Counter from './Components/Counter'
+import FunctionClick from './Components/FunctionClick'
 function App() {
   return (
     <div className="App">
+      <FunctionClick />
       <Counter />
       <Greet />
       <Tagline name = "Travel Agency" subName = "Public Authority">
