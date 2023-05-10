23c17f6e02b07476eb9a22ee5f0de128b63c9818
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-05-10 20:55:58 +0600

    Function Practice

--- intermediate/src/App.js
+++ intermediate/src/App.js
@@ -3,6 +3,7 @@ import './Components/Player.css'
 import Player from './Components/Player.js';
 import Data from './Data.json'
 import List from './List/List.js'
+import Match from './Components/Match.js'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1>Football</h1>
         <Player playerName = {Data[0].Player} playerClub = {Data[0].Club} />
         <List />
+        <Match company="Star Sports" type="Limited Company" />
     </div>
   );
 }
---
+++ intermediate/src/Components/Match.js
@@ -0,0 +1,10 @@
+function Match(props){
+    return (
+    <div>
+        <h3>Chosen channel is {props.company}</h3>
+        <p>This is a {props.type}.</p>
+    </div>
+    )
+}
+
+export default Match;
