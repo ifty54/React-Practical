f11e7d204d5c0147f4d55cb8e040c84d6c8e38d7
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-05-12 00:33:05 +0600

    How to use react icons in your project

--- intermediate/src/App.js
+++ intermediate/src/App.js
@@ -5,11 +5,16 @@ import Data from './Data.json'
 import List from './List/List.js'
 import Match from './Components/Match.js'
 import MatchClass from './Components/MatchClass.js'
+import { AiFillFacebook, AiFillTwitterSquare } from "react-icons/ai";
 
 function App() {
   return (
     <div>
       <h1>Football</h1>
+        <span className='icon'>
+         <AiFillFacebook />
+         <AiFillTwitterSquare />
+        </span>
         <Player playerName = {Data[0].Player} playerClub = {Data[0].Club} />
         <List />
         <Match company="Star Sports" type="Limited Company" />
@@ -19,3 +24,4 @@ function App() {
 }
 
 export default App;
+
